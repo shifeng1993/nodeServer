ec33d78f667965e3be5c3d46f64788060c05c3a1
shifeng <shifeng199307@gmail.com>
2017-06-22 13:54:38 +0800

    6.22

--- app.js
+++ app.js
@@ -2,27 +2,28 @@
 //
 /****************************定义和引入********************************/
 // 引入模块
-var http = require('http');
-var express = require('express');
-var app = express();
+var app = require('express')();
 var bodyParser = require("body-parser");
-var superagent = require('superagent'); //http://visionmedia.github.io/superagent/#response-properties 文档地址
-var server = http.Server(app);
-var host = 'http://localhost:';
-var port = 9999; //设置本地转发服务端口
+var proxy = require('express-http-proxy');
 var Mock = require('mockjs')
+var host = 'http://localhost:';
 
 /*************************以下为设置和启用*****************************/
 // 设置node服务
-app.set('port', port);
+app.set('port', 8888);
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
+app.use('/proxy', proxy('localhost:9999', {
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}));
 
 // 启动服务
-server.listen(app.get('port'), function() {
-  console.log("服务已经启动，APIhost：" + host + port);
+app.listen(app.get('port'), function() {
+  console.log("服务已经启动，APIhost：" + host + app.get('port'));
 });
 
 /******************以下为此服务支持跨域请求********************/
@@ -30,9 +31,9 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, PATCH');
-  res.set({
-    'Content-Type': 'Content-Type:application/json; charset=UTF-8',
-  })
+  // res.set({
+  //   'Content-Type': 'Content-Type:application/json; charset=UTF-8',
+  // })
   if (req.method == 'OPTIONS') {
     res.send(200);
   } else {
@@ -40,104 +41,8 @@ app.all('*', function(req, res, next) {
   }
 });
 
-/******************以下是api公共部分，不用修改***********************/
-// form 请求
-var form = function(req, res, API, log) {
-  var sreq = superagent.post(dbUrl + API);
-  sreq.type('form')
-  sreq.send(req.body);
-  sreq.pipe(res);
-  sreq.on('end', function() {
-    console.log(log);
-  });
-}
+/******************以下是api部分***********************/
 
-// json 请求
-var json = function(req, res, API, log) {
-  var sreq = superagent.post(dbUrl + API);
-  sreq.type('json')
-  sreq.send(req.body);
-  sreq.pipe(res);
-  sreq.on('end', function() {
-    console.log(log);
-  });
-}
-
-/******************以下是api私有部分，必须修改***********************/
-
-// 设置后端服务器url
-var dbUrl = 'http://test.com'; //这里写你的后端api地址
-
-//需配置部分
-// app.get('这里写node服务接受前端发送参数的地址', function(req, res) {
-//   var API = '这里写后端java公共api，当然你们后端是什么的随意，，这里只是举个例子。'
-//   var log = '这里写node服务输出logs';
-//   form(req, res, API, log)
-// });
-
-//以下是不同请求发送方式
-
-// form请求示例
-app.get('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  form(req, res, API, log)
-});
-
-app.post('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  form(req, res, API, log)
-});
-
-app.put('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  form(req, res, API, log)
-});
-
-app.patch('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  form(req, res, API, log)
-});
-
-app.delete('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  form(req, res, API, log)
-});
-
-// json请求示例
-app.get('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  json(req, res, API, log)
-});
-
-app.post('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  json(req, res, API, log)
-});
-
-app.put('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  json(req, res, API, log)
-});
-
-app.patch('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  json(req, res, API, log)
-});
-
-app.delete('/api/machine/init', function(req, res) {
-  var API = '/api/v2/machine/init'
-  var log = '初始化机器';
-  json(req, res, API, log)
-});
 
 // mock数据示例
 app.get('/goods/brand', function(req, res) {
